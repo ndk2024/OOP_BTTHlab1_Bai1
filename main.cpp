1f4335e3505e589f4983d15b5a6d57efb708d2b8
ndk2024 <24520820@gm.uit.edu.vn>
2025-03-06 00:01:59 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,157 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+//B�i 4, B�i 5
 
+struct SinhVien
+{
+    string ten;
+    double toan, van;
+
+};
 struct Ngay
 {
-    int ngay;
-    int thang;
-    int nam;
+    int ngay, thang, nam;
 };
-struct SoTietKiem
+void NhapNgay(Ngay &n)
 {
-    string ms;// Mã sổ
-    string ltk; // Loại tiết kiệm
-    string ten; // Họ tên khách hàng
-    int cmnd; // chứng minh nhân dân
-    Ngay nms;// Ngày mở sổ
-    double sotien;// Số tiền gửi
-};
+    cin>>n.ngay>>n.thang>>n.nam;
+}
+void NhapSinhVien( SinhVien &a)
+{
+    getline(cin, a.ten);
+    cin>>a.toan>>a.van;
+}
+Ngay NgayKT (Ngay a)
+{
+    Ngay b;
+    if(a.nam %4==0)
+    {
+        if(a.thang==1|| a.thang==3||a.thang==5||a.thang==7||a.thang==8||a.thang==10)
+        {
+            if(a.ngay== 31)
+            {
+                b.ngay=1;
+                b.thang=a.thang+1;
+                b.nam=a.nam;
+            }
+            else
+            {
+                b.ngay=a.ngay+1;
+                b.thang=a.thang;
+                b.nam=a.nam;
+            }
+        }
+        else if (a.thang==2)
+        {
+            if(a.ngay==29)
+            {
+                b.ngay=1;
+                b.thang=3;
+                b.nam=a.nam;
+            }
+            else
+            {
+                b.ngay=a.ngay+1;
+                b.thang=2;
+                b.nam=a.nam;
+            }
+        }
+        else if (a.thang==12)
+        {
+            if(a.ngay==31)
+            {
+                b.ngay=1;
+                b.thang=1;
+                b.nam=a.nam+1;
+            }
+        }
+        else
+        {
+            if(a.ngay== 30)
+            {
+                b.ngay=1;
+                b.thang=a.thang+1;
+                b.nam=a.nam;
+            }
+            else
+            {
+                b.ngay=a.ngay+1;
+                b.thang=a.thang;
+                b.nam=a.nam;
+            }
+        }
+    }
+    else
+    {
+        if(a.thang==1|| a.thang==3||a.thang==5||a.thang==7||a.thang==8||a.thang==10)
+        {
+            if(a.ngay== 31)
+            {
+                b.ngay=1;
+                b.thang=a.thang+1;
+                b.nam=a.nam;
+            }
+            else
+            {
+                b.ngay=a.ngay+1;
+                b.thang=a.thang;
+                b.nam=a.nam;
+            }
+        }
+        else if (a.thang==2)
+        {
+            if(a.ngay==28)
+            {
+                b.ngay=1;
+                b.thang=3;
+                b.nam=a.nam;
+            }
+            else
+            {
+                b.ngay=a.ngay+1;
+                b.thang=2;
+                b.nam=a.nam;
+            }
+        }
+        else if (a.thang==12)
+        {
+            if(a.ngay==31)
+            {
+                b.ngay=1;
+                b.thang=1;
+                b.nam=a.nam+1;
+            }
+        }
+        else
+        {
+            if(a.ngay== 30)
+            {
+                b.ngay=1;
+                b.thang=a.thang+1;
+                b.nam=a.nam;
+            }
+            else
+            {
+                b.ngay=a.ngay+1;
+                b.thang=a.thang;
+                b.nam=a.nam;
+            }
+        }
+    }
+    return b;
+}
 int main()
 {
-
+    SinhVien a;
+    NhapSinhVien(a);
+    Ngay ngay1, ngaykt;
+    NhapNgay(ngay1);
+    ngaykt=NgayKT(ngay1);
+    //B�i 4
+    cout<<"Bai 4: "<<ngaykt.ngay<<"/"<<ngaykt.thang<<"/"<<ngaykt.nam<<endl;
+    //B�i 5
+    cout<<"Bai 5: ";
+    cout<<(a.toan+a.van)/2<<endl;
     return 0;
 }
