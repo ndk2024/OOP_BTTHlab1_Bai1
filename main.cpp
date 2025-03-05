98640f1f904e181138607e4677c7eb5476fc6aab
ndk2024 <24520820@gm.uit.edu.vn>
2025-03-05 23:15:39 +0700

    Bai tap 8

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Ngay
+{
+    int ngay;
+    int thang;
+    int nam;
+};
+struct SoTietKiem
+{
+    string ms;// Mã sổ
+    string ltk; // Loại tiết kiệm
+    string ten; // Họ tên khách hàng
+    int cmnd; // chứng minh nhân dân
+    Ngay nms;// Ngày mở sổ
+    double sotien;// Số tiền gửi
+};
+int main()
+{
+
+    return 0;
+}
